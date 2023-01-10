a689f3fc8424b863c7a3578a96e5162838d440af
Virag Kormoczy <vkormoczy@gmail.com>
2023-01-10 13:39:17 +0200

    Connect frontend with backend

--- README.md
+++
@@ -1,8 +0,0 @@
-# FullStackOpen-Part3
-
-> This repository contains my exercise submissions for the [Full Stack Open](https://fullstackopen.com/en/about) course from Part 3. 
-
-> The course introduces modern web application development using JavaScript. The main focus is on single page applications implemented using the React library, and the REST interfaces implemented with Node.js that support them. The course also introduces you to the interface implementation principle of GraphQL, i.e. an alternative to REST.
-
-> The course also covers application testing, configuration and management of execution environments, as well as the use of the MongoDB database to store the application's data.
-
---
+++ server/index.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static('build'));
+
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, 'access.log'),
+  { flags: 'a' }
+);
+
+app.use(morgan(':method :url :status :json', { stream: accessLogStream }));
+
+morgan.token('json', function (req, res) {
+  return JSON.stringify(req.body);
+});
+
+let persons = [
+  {
+    id: 1,
+    name: 'Michael Scott',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Jim Halpert',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dwight Schrute',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Pam Beesly',
+    number: '39-23-6423122',
+  },
+];
+
+// Generate ID
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+// Home page
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>');
+});
+
+// Get all persons
+app.get('/api/persons', (request, response) => {
+  response.json(persons);
+});
+
+// Display info
+app.get('/info', (request, response) => {
+  const info = `<p>Phonebook has info for ${persons.length} people.</p>
+  <br/>
+  <p>${new Date()}</p>
+  `;
+
+  response.send(info);
+});
+
+// Get person by id
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  person ? response.json(person) : response.status(404).end();
+});
+
+// Delete person
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
+// Create person
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'Name is missing',
+    });
+  } else if (!body.number) {
+    return response.status(400).json({
+      error: 'Number is missing',
+    });
+  }
+
+  const newPerson = {
+    name: body.name,
+    number: body.number,
+    date: new Date(),
+    id: generateId(),
+  };
+
+  if (persons.includes(newPerson.content)) {
+    return response.status(400).json({
+      error: 'Name already exist',
+    });
+  }
+  persons = persons.concat(newPerson);
+  response.json(newPerson);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

87227cfe369700730bd23f184fc0eb33c7cf228b
Virag Kormoczy <vkormoczy@gmail.com>
2023-01-25 16:03:37 +0200

    uibuild

--- server/models/person.js
+++ server/models/person.js
@@ -18,18 +18,18 @@ mongoose
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
-    minLength: 3,
-    required: true,
+    minLength: [3, 'Name must be minimum 3 characters long'],
+    required: [true, 'Name is required'],
   },
   number: {
     type: String,
-    minLength: 8,
+    minLength: [8, 'Phone number must be at least 8 characters long'],
     validate: {
       validator: function (v) {
-        return /\d{2,3}-\d{5,}/.test(v);
+        return /^\d{2}\d?-\d{5,}/.test(v);
       },
       message: (props) =>
-        `${props.value} is not a valid phone number! The phone number must be in one of this format: xx-xxxxxx or xxx-xxxxx, minimum 8 characters.`,
+        `${props.value} is not a valid phone number! The phone number must be in one of this format: xx-xxxxxx or xxx-xxxxx`,
     },
     required: [true, 'Phone number is required'],
   },
